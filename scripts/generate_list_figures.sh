2d8232b46a5e5c48f7fa29e53f703c83b522fa63
Eduardo Alberto Duarte Lacerda <lacerda@astro.unam.mx>
2019-02-05 17:34:01 -0600

    scripts/generate_list_figures.sh: added new figs

--- scripts/generate_list_figures.sh
+++ scripts/generate_list_figures.sh
@@ -4,4 +4,6 @@ pdftk fig_BPT.pdf fig_histo_CMD_CUBES.pdf fig_histo_CMD_NSA.pdf fig_SFMS.pdf \
     fig_histo_M_C.pdf fig_M_sSFR.pdf fig_histo_M_sSFR.pdf fig_sSFR_C.pdf \
     fig_histo_sSFR_C.pdf fig_Morph_M.pdf fig_Morph_C.pdf \
     fig_Morph_SigmaMassCen.pdf fig_Morph_vsigma.pdf fig_Morph_Re.pdf \
+    fig_histo_M_tLW.pdf fig_histo_M_tMW.pdf fig_histo_M_ZHLW.pdf \
+    fig_histo_M_ZHMW.pdf fig_histo_MZR.pdf \
     cat output allfigs.pdf
