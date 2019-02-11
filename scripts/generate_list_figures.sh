ec8104c137a94a731f00e3b0ca2b973fca7399d1
Eduardo Alberto Duarte Lacerda <lacerda@astro.unam.mx>
2019-02-11 15:30:46 -0600

    Added qpdf use if pdtfk do not exists

--- scripts/generate_list_figures.sh
+++ scripts/generate_list_figures.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-pdftk fig_BPT.pdf fig_histo_CMD_CUBES.pdf fig_histo_CMD_NSA.pdf fig_SFMS.pdf \
-    fig_histo_SFMS.pdf fig_SFMS_NC.pdf fig_histo_SFMS_NC.pdf fig_M_C.pdf \
-    fig_histo_M_C.pdf fig_M_sSFR.pdf fig_histo_M_sSFR.pdf fig_sSFR_C.pdf \
-    fig_histo_sSFR_C.pdf fig_Morph_M.pdf fig_Morph_C.pdf \
-    fig_Morph_SigmaMassCen.pdf fig_Morph_vsigma.pdf fig_Morph_Re.pdf \
-    fig_histo_M_tLW.pdf fig_histo_M_tMW.pdf fig_histo_M_ZHLW.pdf \
-    fig_histo_M_ZHMW.pdf fig_histo_MZR.pdf \
-    cat output allfigs.pdf
+FIGS_DIR=${HOME}/dev/astro/AGNs_CALIFA/figs
+ALLFIGS=$FIGS_DIR/allfigs.pdf
+FIGS=()
+if [ -f "$ALLFIGS" ]
+then
+  echo "$0: removing $ALLFIGS"
+  rm $ALLFIGS
+fi
+
+for f in $FIGS_DIR/*.pdf; do
+  FIGS+=( $f )
+done
+
+if [ -z "$( which pdftk )" ]
+then
+  echo "$0: using qpdf instead pdftk"
+  qpdf --empty --pages ${FIGS[*]} -- $ALLFIGS
+else
+  pdftk $FIGS cat output $ALLFIGS
+fi
