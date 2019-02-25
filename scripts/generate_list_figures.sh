de22d6edc69fbea395d2bb911eb4d352aafbbbef
Eduardo Alberto Duarte Lacerda <lacerda@astro.unam.mx>
2019-02-25 08:05:17 -0600

    correct script to generate a pdf with all figs

--- scripts/generate_list_figures.sh
+++ scripts/generate_list_figures.sh
@@ -3,21 +3,28 @@ FIGS_DIR=${HOME}/dev/astro/AGNs_CALIFA/figs
 PUBDIR=${HOME}/public_html/AGNs_CALIFA/
 ALLFIGS=$FIGS_DIR/allfigs.pdf
 FIGS=()
+
+# Remove old ALLFIGS file
 if [ -f "$ALLFIGS" ]
 then
   echo "$0: removing $ALLFIGS"
   rm $ALLFIGS
 fi
 
-for f in $FIGS_DIR/*.pdf; do
+# Search for all pdf files
+for f in $FIGS_DIR/*.pdf
+do
   FIGS+=( $f )
 done
 
+# Generate a pdf with all figs
 if [ -z "$( which pdftk )" ]
 then
   echo "$0: using qpdf instead pdftk"
   qpdf --empty --pages ${FIGS[*]} -- $ALLFIGS
 else
-  pdftk $FIGS cat output $ALLFIGS
+  pdftk ${FIGS[*]} cat output $ALLFIGS
 fi
+
+# Publish the images on localhost
 cp -r ${FIGS_DIR} ${PUBDIR}
